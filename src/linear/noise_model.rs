000003da114a92552c947aa41235b402f13a5207
Fan Jiang <i@fanjiang.me>
2019-04-17 16:23:26 +0800

    Notice that mat.try_inverse() is mutating

--- src/linear/noise_model.rs
+++ src/linear/noise_model.rs
@@ -2,12 +2,12 @@ use nalgebra::base::allocator::Allocator;
 use nalgebra::base::default_allocator::DefaultAllocator;
 use nalgebra::base::dimension::Dim;
 use nalgebra::base::storage::Storage;
-use nalgebra::base::{DMatrix, DVector, MatrixMN, VectorN};
+use nalgebra::base::{DMatrix, DVector, MatrixN, VectorN};
 use nalgebra::RealField;
 use std::fmt::Debug;
 
 #[allow(non_snake_case)]
-pub trait NoiseModel<D1: Dim, D2: Dim, T: RealField = f64>: Debug {
+pub trait NoiseModel<D: Dim, T: RealField = f64>: Debug {
     fn is_constrained(&self) -> bool;
 
     fn is_unit(&self) -> bool;
@@ -16,21 +16,21 @@ pub trait NoiseModel<D1: Dim, D2: Dim, T: RealField = f64>: Debug {
 
     fn sigmas(&self) -> DVector<T>;
 
-    fn whiten(&self, v: &VectorN<T, D1>) -> VectorN<T, D1>
+    fn whiten(&self, v: &VectorN<T, D>) -> VectorN<T, D>
     where
-        DefaultAllocator: Allocator<T, D1>;
+        DefaultAllocator: Allocator<T, D>;
 
-    fn whiten_mat(&self, m: &MatrixMN<T, D1, D2>) -> MatrixMN<T, D1, D2>
+    fn whiten_mat(&self, m: &MatrixN<T, D>) -> MatrixN<T, D>
     where
-        DefaultAllocator: Allocator<T, D1, D2>;
+        DefaultAllocator: Allocator<T, D, D>;
 
-    fn unwhiten(&self, v: &VectorN<T, D1>) -> VectorN<T, D1>
+    fn unwhiten(&self, v: &VectorN<T, D>) -> VectorN<T, D>
     where
-        DefaultAllocator: Allocator<T, D1>;
+        DefaultAllocator: Allocator<T, D>;
 
-    fn distance(&self, v: &VectorN<T, D1>) -> T
+    fn distance(&self, v: &VectorN<T, D>) -> T
     where
-        DefaultAllocator: Allocator<T, D1>;
+        DefaultAllocator: Allocator<T, D>;
 
     fn whiten_system<_D: Dim>(&self, A: &[DMatrix<T>], b: &VectorN<T, _D>)
     where
@@ -38,32 +38,30 @@ pub trait NoiseModel<D1: Dim, D2: Dim, T: RealField = f64>: Debug {
 }
 
 #[allow(non_snake_case)]
-pub trait GaussianNoise<D1: Dim, D2: Dim, T: RealField = f64>: NoiseModel<D1, D2, T> {
-    fn from_sqrtinfo(R: &MatrixMN<T, D1, D2>, smart: bool) -> Self
+pub trait GaussianNoise<D: Dim, T: RealField = f64>: NoiseModel<D, T> {
+    fn from_sqrtinfo(R: &MatrixN<T, D>, smart: bool) -> Self
     where
-        DefaultAllocator: Allocator<T, D1, D2>;
+        DefaultAllocator: Allocator<T, D, D>;
 
-    fn from_information(info: &MatrixMN<T, D1, D2>, smart: bool) -> Self
+    fn from_information(info: &MatrixN<T, D>, smart: bool) -> Self
     where
-        DefaultAllocator: Allocator<T, D1, D2>;
+        DefaultAllocator: Allocator<T, D, D>;
 
-    fn from_covariance(cov: &MatrixMN<T, D1, D2>, smart: bool) -> Self
+    fn from_covariance(cov: &MatrixN<T, D>, smart: bool) -> Self
     where
-        DefaultAllocator: Allocator<T, D1, D2>;
+        DefaultAllocator: Allocator<T, D, D>;
 
-    fn sqrt_info(&self) -> Option<&MatrixMN<T, D1, D2>>
+    fn sqrt_info(&self) -> Option<&MatrixN<T, D>>
     where
-        DefaultAllocator: Allocator<T, D1, D2>;
+        DefaultAllocator: Allocator<T, D, D>;
 
     /// Mahalanobis distance v'*R'*R*v = <R*v,R*v>
     fn mahalanobis_dist(&self, v: &DVector<T>) -> T;
 }
 
-fn check_diagonal_upper<D1: Dim, D2: Dim, T: nalgebra::RealField>(
-    mat: &MatrixMN<T, D1, D2>,
-) -> Option<DVector<T>>
+fn check_diagonal_upper<D: Dim, T: nalgebra::RealField>(mat: &MatrixN<T, D>) -> Option<DVector<T>>
 where
-    DefaultAllocator: Allocator<T, D1, D2>,
+    DefaultAllocator: Allocator<T, D, D>,
 {
     let (m, n) = (mat.nrows(), mat.ncols());
     let mut full = false;
@@ -90,26 +88,28 @@ where
 }
 
 #[derive(Debug)]
-pub struct Gaussian<D1: Dim, D2: Dim, T: RealField = f64>
+pub struct Gaussian<D: Dim, T: RealField = f64>
 where
-    DefaultAllocator: Allocator<T, D1, D2>,
+    DefaultAllocator: Allocator<T, D, D>,
 {
     dim: usize,
-    sqrt_info: Option<MatrixMN<T, D1, D2>>,
+    sqrt_info: Option<MatrixN<T, D>>,
 }
 
-impl<D1: Dim, D2: Dim, T: RealField> GaussianNoise<D1, D2, T> for Gaussian<D1, D2, T>
+impl<D: Dim, T: RealField> GaussianNoise<D, T> for Gaussian<D, T>
 where
-    DefaultAllocator: Allocator<T, D1, D2>,
+    DefaultAllocator: Allocator<T, D, D>,
 {
-    fn from_sqrtinfo(R: &MatrixMN<T, D1, D2>, smart: bool) -> Self
+    fn from_sqrtinfo(R: &MatrixN<T, D>, smart: bool) -> Self
     where
-        DefaultAllocator: Allocator<T, D1, D2>,
+        DefaultAllocator: Allocator<T, D, D>,
     {
         let (m, n) = (R.nrows(), R.ncols());
         assert_eq!(m, n, "Non-square Matrix");
         if smart {
-            if let Some(diagonal) = check_diagonal_upper(&R) {}
+            if let Some(diagonal) = check_diagonal_upper(&R) {
+                unimplemented!();
+            }
         }
 
         Gaussian {
@@ -118,23 +118,36 @@ where
         }
     }
 
-    fn from_information(info: &MatrixMN<T, D1, D2>, smart: bool) -> Self
+    fn from_information(info: &MatrixN<T, D>, smart: bool) -> Self
     where
-        DefaultAllocator: Allocator<T, D1, D2>,
+        DefaultAllocator: Allocator<T, D, D>,
     {
         unimplemented!()
     }
 
-    fn from_covariance(cov: &MatrixMN<T, D1, D2>, smart: bool) -> Self
+    fn from_covariance(cov: &MatrixN<T, D>, smart: bool) -> Self
     where
-        DefaultAllocator: Allocator<T, D1, D2>,
+        DefaultAllocator: Allocator<T, D, D>,
     {
-        unimplemented!()
+        let (m, n) = (cov.nrows(), cov.ncols());
+        assert_eq!(m, n, "Non-square Matrix");
+        if smart {
+            if let Some(diagonal) = check_diagonal_upper(cov) {
+                unimplemented!();
+            }
+        }
+
+        // NOTE: if cov = L'*L, then the square root information R can be found by
+        // QR, as L.inverse() = Q*R, with Q some rotation matrix. However, R has
+        // annoying sign flips with respect the simpler Information(inv(cov)),
+        // hence we choose the simpler path here:
+        let inv = cov.clone().try_inverse();
+        Gaussian::from_information(&inv.unwrap(), false)
     }
 
-    fn sqrt_info(&self) -> Option<&MatrixMN<T, D1, D2>>
+    fn sqrt_info(&self) -> Option<&MatrixN<T, D>>
     where
-        DefaultAllocator: Allocator<T, D1, D2>,
+        DefaultAllocator: Allocator<T, D, D>,
     {
         if let Some(s) = &self.sqrt_info {
             return Some(s);
@@ -150,9 +163,9 @@ where
     }
 }
 
-impl<D1: Dim, D2: Dim, T: RealField> NoiseModel<D1, D2, T> for Gaussian<D1, D2, T>
+impl<D: Dim, T: RealField> NoiseModel<D, T> for Gaussian<D, T>
 where
-    DefaultAllocator: Allocator<T, D1, D2>,
+    DefaultAllocator: Allocator<T, D, D>,
 {
     fn is_constrained(&self) -> bool {
         unimplemented!()
@@ -170,37 +183,37 @@ where
         unimplemented!()
     }
 
-    fn whiten(&self, v: &VectorN<T, D1>) -> VectorN<T, D1>
+    fn whiten(&self, v: &VectorN<T, D>) -> VectorN<T, D>
     where
-        DefaultAllocator: Allocator<T, D1, D2> + Allocator<T, D1>,
+        DefaultAllocator: Allocator<T, D, D> + Allocator<T, D>,
     {
         unimplemented!()
     }
 
-    fn whiten_mat(&self, m: &MatrixMN<T, D1, D2>) -> MatrixMN<T, D1, D2>
+    fn whiten_mat(&self, m: &MatrixN<T, D>) -> MatrixN<T, D>
     where
-        DefaultAllocator: Allocator<T, D1, D2>,
+        DefaultAllocator: Allocator<T, D, D>,
     {
         unimplemented!()
     }
 
-    fn unwhiten(&self, v: &VectorN<T, D1>) -> VectorN<T, D1>
+    fn unwhiten(&self, v: &VectorN<T, D>) -> VectorN<T, D>
     where
-        DefaultAllocator: Allocator<T, D1, D2> + Allocator<T, D1>,
+        DefaultAllocator: Allocator<T, D, D> + Allocator<T, D>,
     {
         unimplemented!()
     }
 
-    fn distance(&self, v: &VectorN<T, D1>) -> T
+    fn distance(&self, v: &VectorN<T, D>) -> T
     where
-        DefaultAllocator: Allocator<T, D1, D2> + Allocator<T, D1>,
+        DefaultAllocator: Allocator<T, D, D> + Allocator<T, D>,
     {
         unimplemented!()
     }
 
     fn whiten_system<_D: Dim>(&self, A: &[DMatrix<T>], b: &VectorN<T, _D>)
     where
-        DefaultAllocator: Allocator<T, D1, D2> + Allocator<T, _D>,
+        DefaultAllocator: Allocator<T, D, D> + Allocator<T, _D>,
     {
         unimplemented!()
     }
